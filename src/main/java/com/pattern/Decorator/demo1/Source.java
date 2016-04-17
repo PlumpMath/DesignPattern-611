0a0eb983d2097bcf7e89d8478dd287765cd449c6
sunxuechao <sunxuechao@SUN-PC>
2016-04-17 11:56:37 +0800

    decorator

---
+++ src/main/java/com/pattern/Decorator/demo1/Source.java
@@ -0,0 +1,8 @@
+package com.pattern.Decorator.demo1;
+public class Source implements Sourceable {  
+  
+    @Override  
+    public void method() {  
+        System.out.println("the original method!");  
+    }  
+} 
---
+++ src/main/java/com/pattern/Decorator/demo1/Sourceable.java
@@ -0,0 +1,9 @@
+package com.pattern.Decorator.demo1;
+/**
+ * Source类是被装饰类，Decorator类是一个装饰类，可以为Source类动态的添加一些功能
+ * @author sunxuechao
+ *
+ */
+public interface Sourceable {  
+    public void method();  
+} 
